34323ffae58d51eca7ea4912ade913eeb3fd8f90
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-05-19 23:15:08 +0200

    fix: typo

--- src/app/workspace/settings/page.tsx
+++ src/app/workspace/settings/page.tsx
@@ -1,7 +1,7 @@
 export default function Example() {
     return (
         <>
-            <p className="text-sm text-gray-500">content here</p>
+            <p className="text-sm text-gray-500">[/settings should not be publicly exposed, if clicked forward to /settings/general]</p>
         </>
     )
 }
