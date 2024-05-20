b2bda078faf73871e1ef7653079f1ed4d96a1d57
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-05-20 14:46:53 +0200

    chore: redirect home

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+// @SEV
 const nextConfig = {
-    redirects: async () => {
-        return [
-            { 
-                source: '/workspace/settings', 
-                destination: '/workspace/settings/general', 
-                permanent: true, 
-            }, 
-        ];
-    }
+  redirects: async () => {
+    return [
+      {
+        source: "/workspace/settings",
+        destination: "/workspace/settings/general",
+        permanent: true,
+      },
+      {
+        source: "/",
+        destination: "/workspace/overview",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
