bfcd696331384610fef4a661c2c9c0ef231fa28c
severinlandolt <sev.landolt@gmail.com>
2024-05-29 19:35:31 +0200

    first draft

---
+++ src/components/ui/data-table/data-table-view-options-new.tsx
@@ -0,0 +1,494 @@
+"use client";
+import React, {
+  createContext,
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/Popover";
+import { Table } from "@tanstack/react-table";
+
+import ReactDOM from "react-dom";
+import invariant from "tiny-invariant";
+
+import { triggerPostMoveFlash } from "@atlaskit/pragmatic-drag-and-drop-flourish/trigger-post-move-flash";
+import {
+  attachClosestEdge,
+  type Edge,
+  extractClosestEdge,
+} from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge";
+import { getReorderDestinationIndex } from "@atlaskit/pragmatic-drag-and-drop-hitbox/util/get-reorder-destination-index";
+import * as liveRegion from "@atlaskit/pragmatic-drag-and-drop-live-region";
+import { DropIndicator } from "@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box";
+import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
+import {
+  draggable,
+  dropTargetForElements,
+  monitorForElements,
+} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { pointerOutsideOfPreview } from "@atlaskit/pragmatic-drag-and-drop/element/pointer-outside-of-preview";
+import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
+import { reorder } from "@atlaskit/pragmatic-drag-and-drop/reorder";
+import { Button } from "@/components/Button";
+import { RiDraggable, RiEqualizer2Line } from "@remixicon/react";
+import { cx } from "@/lib/utils";
+import { Label } from "@/components/Label";
+import { Checkbox } from "@/components/Checkbox";
+
+type ItemPosition = "first" | "last" | "middle" | "only";
+
+type CleanupFn = () => void;
+
+type ItemEntry = { itemId: string; element: HTMLElement };
+
+type ListContextValue = {
+  getListLength: () => number;
+  registerItem: (entry: ItemEntry) => CleanupFn;
+  reorderItem: (args: {
+    startIndex: number;
+    indexOfTarget: number;
+    closestEdgeOfTarget: Edge | null;
+  }) => void;
+  instanceId: symbol;
+};
+
+const ListContext = createContext<ListContextValue | null>(null);
+
+function useListContext() {
+  const listContext = useContext(ListContext);
+  invariant(listContext !== null);
+  return listContext;
+}
+
+type Item = {
+  id: string;
+  label: string;
+};
+
+
+const itemKey = Symbol("item");
+
+type ItemData = {
+  [itemKey]: true;
+  item: Item;
+  index: number;
+  instanceId: symbol;
+};
+
+function getItemData({
+  item,
+  index,
+  instanceId,
+}: {
+  item: Item;
+  index: number;
+  instanceId: symbol;
+}): ItemData {
+  return {
+    [itemKey]: true,
+    item,
+    index,
+    instanceId,
+  };
+}
+
+function isItemData(data: Record<string | symbol, unknown>): data is ItemData {
+  return data[itemKey] === true;
+}
+
+function getItemPosition({
+  index,
+  items,
+}: {
+  index: number;
+  items: Item[];
+}): ItemPosition {
+  if (items.length === 1) {
+    return "only";
+  }
+
+  if (index === 0) {
+    return "first";
+  }
+
+  if (index === items.length - 1) {
+    return "last";
+  }
+
+  return "middle";
+}
+
+type DraggableState =
+  | { type: "idle" }
+  | { type: "preview"; container: HTMLElement }
+  | { type: "dragging" };
+
+const idleState: DraggableState = { type: "idle" };
+const draggingState: DraggableState = { type: "dragging" };
+
+function ListItem({
+  item,
+  index,
+}: {
+  item: Item;
+  index: number;
+  position: ItemPosition;
+}) {
+  const { registerItem, instanceId } = useListContext();
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [closestEdge, setClosestEdge] = useState<Edge | null>(null);
+
+  const dragHandleRef = useRef<HTMLButtonElement>(null);
+
+  const [draggableState, setDraggableState] =
+    useState<DraggableState>(idleState);
+
+  useEffect(() => {
+    const element = ref.current;
+    const dragHandle = dragHandleRef.current;
+    invariant(element);
+    invariant(dragHandle);
+
+    const data = getItemData({ item, index, instanceId });
+
+    return combine(
+      registerItem({ itemId: item.id, element }),
+      draggable({
+        element: dragHandle,
+        getInitialData: () => data,
+        onGenerateDragPreview({ nativeSetDragImage }) {
+          setCustomNativeDragPreview({
+            nativeSetDragImage,
+            getOffset: pointerOutsideOfPreview({
+              x: "10px",
+              y: "10px",
+            }),
+            render({ container }) {
+              setDraggableState({ type: "preview", container });
+
+              return () => setDraggableState(draggingState);
+            },
+          });
+        },
+        onDragStart() {
+          setDraggableState(draggingState);
+        },
+        onDrop() {
+          setDraggableState(idleState);
+        },
+      }),
+      dropTargetForElements({
+        element,
+        canDrop({ source }) {
+          return (
+            isItemData(source.data) && source.data.instanceId === instanceId
+          );
+        },
+        getData({ input }) {
+          return attachClosestEdge(data, {
+            element,
+            input,
+            allowedEdges: ["top", "bottom"],
+          });
+        },
+        onDrag({ self, source }) {
+          const isSource = source.element === element;
+          if (isSource) {
+            setClosestEdge(null);
+            return;
+          }
+
+          const closestEdge = extractClosestEdge(self.data);
+
+          const sourceIndex = source.data.index;
+          invariant(typeof sourceIndex === "number");
+
+          const isItemBeforeSource = index === sourceIndex - 1;
+          const isItemAfterSource = index === sourceIndex + 1;
+
+          const isDropIndicatorHidden =
+            (isItemBeforeSource && closestEdge === "bottom") ||
+            (isItemAfterSource && closestEdge === "top");
+
+          if (isDropIndicatorHidden) {
+            setClosestEdge(null);
+            return;
+          }
+
+          setClosestEdge(closestEdge);
+        },
+        onDragLeave() {
+          setClosestEdge(null);
+        },
+        onDrop() {
+          setClosestEdge(null);
+        },
+      })
+    );
+  }, [instanceId, item, index, registerItem]);
+
+  return (
+    <Fragment>
+      <div ref={ref} className="relative border-b py-1">
+        <div
+          className={cx(
+            "relative flex items-center justify-between gap-2",
+            draggableState.type === "dragging" && "opacity-50"
+          )}
+        >
+          <div className="flex items-center gap-2">
+            <Checkbox
+              // checked={column.getIsVisible()}
+              checked={true}
+              // onCheckedChange={() => column.toggleVisibility()}
+            />
+            <span>{item.label}</span>
+          </div>
+
+          <Button
+            variant="secondary"
+            className="px-px py-1"
+            ref={dragHandleRef}
+            aria-label={`Reorder ${item.label}`}
+          >
+            <RiDraggable className="text-gray-400 dark:text-gray-600 size-3.5" />
+          </Button>
+        </div>
+        {closestEdge && <DropIndicator edge={closestEdge} />}
+      </div>
+      {draggableState.type === "preview" &&
+        ReactDOM.createPortal(
+          <div>{item.label}</div>,
+          draggableState.container
+        )}
+    </Fragment>
+  );
+}
+
+const defaultItems: Item[] = [
+  {
+    id: "task-1",
+    label: "Organize",
+  },
+  {
+    id: "task-2",
+    label: "Create",
+  },
+  {
+    id: "task-3",
+    label: "Update",
+  },
+  {
+    id: "task-4",
+    label: "Plan",
+  },
+  {
+    id: "task-5",
+    label: "Coordinate",
+  },
+  {
+    id: "task-6",
+    label: "Manage",
+  },
+];
+
+function getItemRegistry() {
+  const registry = new Map<string, HTMLElement>();
+
+  function register({ itemId, element }: ItemEntry) {
+    registry.set(itemId, element);
+
+    return function unregister() {
+      registry.delete(itemId);
+    };
+  }
+
+  function getElement(itemId: string): HTMLElement | null {
+    return registry.get(itemId) ?? null;
+  }
+
+  return { register, getElement };
+}
+
+type ListState = {
+  items: Item[];
+  lastCardMoved: {
+    item: Item;
+    previousIndex: number;
+    currentIndex: number;
+    numberOfItems: number;
+  } | null;
+};
+
+interface DataTableViewOptionsProps<TData> {
+    table: Table<TData>;
+  }
+
+export default function ListExample<TData>({
+    table,
+  }: DataTableViewOptionsProps<TData>) {
+  const [{ items, lastCardMoved }, setListState] = useState<ListState>({
+    items: defaultItems,
+    lastCardMoved: null,
+  });
+  const [registry] = useState(getItemRegistry);
+
+  // Isolated instances of this component from one another
+  const [instanceId] = useState(() => Symbol("instance-id"));
+
+  const reorderItem = useCallback(
+    ({
+      startIndex,
+      indexOfTarget,
+      closestEdgeOfTarget,
+    }: {
+      startIndex: number;
+      indexOfTarget: number;
+      closestEdgeOfTarget: Edge | null;
+    }) => {
+      const finishIndex = getReorderDestinationIndex({
+        startIndex,
+        closestEdgeOfTarget,
+        indexOfTarget,
+        axis: "vertical",
+      });
+
+      if (finishIndex === startIndex) {
+        return;
+      }
+
+      setListState((listState) => {
+        const item = listState.items[startIndex];
+
+        return {
+          items: reorder({
+            list: listState.items,
+            startIndex,
+            finishIndex,
+          }),
+          lastCardMoved: {
+            item,
+            previousIndex: startIndex,
+            currentIndex: finishIndex,
+            numberOfItems: listState.items.length,
+          },
+        };
+      });
+    },
+    []
+  );
+
+  useEffect(() => {
+    return monitorForElements({
+      canMonitor({ source }) {
+        return isItemData(source.data) && source.data.instanceId === instanceId;
+      },
+      onDrop({ location, source }) {
+        const target = location.current.dropTargets[0];
+        if (!target) {
+          return;
+        }
+
+        const sourceData = source.data;
+        const targetData = target.data;
+        if (!isItemData(sourceData) || !isItemData(targetData)) {
+          return;
+        }
+
+        const indexOfTarget = items.findIndex(
+          (item) => item.id === targetData.item.id
+        );
+        if (indexOfTarget < 0) {
+          return;
+        }
+
+        const closestEdgeOfTarget = extractClosestEdge(targetData);
+
+        reorderItem({
+          startIndex: sourceData.index,
+          indexOfTarget,
+          closestEdgeOfTarget,
+        });
+      },
+    });
+  }, [instanceId, items, reorderItem]);
+
+  // once a drag is finished, we have some post drop actions to take
+  useEffect(() => {
+    if (lastCardMoved === null) {
+      return;
+    }
+
+    const { item, previousIndex, currentIndex, numberOfItems } = lastCardMoved;
+    const element = registry.getElement(item.id);
+    if (element) {
+      triggerPostMoveFlash(element);
+    }
+
+    liveRegion.announce(
+      `You've moved ${item.label} from position ${
+        previousIndex + 1
+      } to position ${currentIndex + 1} of ${numberOfItems}.`
+    );
+  }, [lastCardMoved, registry]);
+
+  // cleanup the live region when this component is finished
+  useEffect(() => {
+    return function cleanup() {
+      liveRegion.cleanup();
+    };
+  }, []);
+
+  const getListLength = useCallback(() => items.length, [items.length]);
+
+  const contextValue: ListContextValue = useMemo(() => {
+    return {
+      registerItem: registry.register,
+      reorderItem,
+      instanceId,
+      getListLength,
+    };
+  }, [registry.register, reorderItem, instanceId, getListLength]);
+
+  return (
+    <div>
+      <div className="flex justify-center">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant="secondary"
+              className={cx(
+                // focusInput,
+                "ml-auto hidden py-1 px-2 lg:flex gap-x-2 focus:outline-none"
+              )}
+            >
+              <RiEqualizer2Line className="-ml-px size-4" aria-hidden="true" />
+              View options new
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent align="end" className="space-y-2 w-fit">
+            <Label className="font-medium">Display properties</Label>
+            <ListContext.Provider value={contextValue}>
+              <div className="flex flex-col">
+                {items.map((item, index) => (
+                  <ListItem
+                    key={item.id}
+                    item={item}
+                    index={index}
+                    position={getItemPosition({ index, items })}
+                  />
+                ))}
+              </div>
+            </ListContext.Provider>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+}
