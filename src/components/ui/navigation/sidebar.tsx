f11993837986d9f1a4312838f9f2c2d87da1c8de
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-05-31 22:13:25 +0200

    sidebar: add shortcuts to nav

--- src/components/ui/navigation/sidebar.tsx
+++ src/components/ui/navigation/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { siteConfig } from "@/app/siteConfig"
 import { cx, focusRing } from "@/lib/utils"
-import { RiHome2Line, RiListCheck, RiSettings5Line } from "@remixicon/react"
+import { RiHome2Line, RiListCheck, RiSettings5Line, RiTimeLine } from "@remixicon/react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -25,39 +25,85 @@ const navigation = [
   },
 ]
 
+const shortcuts = [
+  // @CHRIS: replace links when anchors are set
+  {
+    name: "Add new user",
+    href: "#",
+    icon: RiTimeLine
+  },
+  {
+    name: "Workspace usage",
+    href: "#",
+    icon: RiTimeLine
+  },
+  {
+    name: "Cost spend control",
+    href: "#",
+    icon: RiTimeLine,
+  },
+  {
+    name: "Revenue by country",
+    href: "#",
+    icon: RiTimeLine,
+  },
+]
+
 export function Sidebar() {
   const pathname = usePathname()
   return (
     <>
       {/* sidebar (lg+) */}
-      <nav className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
-        <aside className="flex grow flex-col gap-y-6 overflow-y-auto border-r border-gray-200 bg-white px-6 py-4 dark:border-gray-800 dark:bg-gray-950">
+      <nav className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <aside className="flex grow flex-col gap-y-6 overflow-y-auto border-r border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-gray-950">
           <div className="">
             <WorkspacesDropdownDesktop />
           </div>
           <nav
             aria-label="core navigation links"
-            className="flex flex-1 flex-col"
+            className="flex flex-1 flex-col space-y-10"
           >
-            <ul role="list" className="space-y-1.5">
+            <ul role="list" className="space-y-0.5">
               {navigation.map((item) => (
                 <li key={item.name}>
                   <Link
                     href={item.href}
                     className={cx(
                       pathname === item.href || pathname.includes(item.href)
-                        ? "text-indigo-600 dark:text-indigo-400"
-                        : "text-gray-600 hover:text-gray-900 dark:text-gray-400 hover:dark:text-gray-50",
-                      "flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-sm font-medium transition hover:bg-gray-100 hover:dark:bg-gray-900",
+                        ? "text-indigo-600 dark:text-indigo-400 font-medium"
+                        : "text-gray-700 hover:text-gray-900 dark:text-gray-400 hover:dark:text-gray-50",
+                      "flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-sm transition hover:bg-gray-100 hover:dark:bg-gray-900",
                       focusRing,
                     )}
                   >
-                    <item.icon className="size-5 shrink-0" aria-hidden="true" />
+                    <item.icon className="size-4 shrink-0" aria-hidden="true" />
                     {item.name}
                   </Link>
                 </li>
               ))}
             </ul>
+            <div>
+              <span className="text-xs font-medium leading-6 text-gray-500">Shortcuts</span>
+              <ul role="list" className="space-y-0.5">
+                {shortcuts.map((item) => (
+                  <li key={item.name}>
+                    <Link
+                      href={item.href}
+                      className={cx(
+                        pathname === item.href || pathname.includes(item.href)
+                          ? "text-indigo-600 dark:text-indigo-400"
+                          : "text-gray-700 hover:text-gray-900 dark:text-gray-400 hover:dark:text-gray-50",
+                        "flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-sm transition hover:bg-gray-100 hover:dark:bg-gray-900",
+                        focusRing,
+                      )}
+                    >
+                      <item.icon className="size-4 shrink-0" aria-hidden="true" />
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </nav>
           <div className="mt-auto">
             <UserProfileDesktop />
