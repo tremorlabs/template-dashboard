dc5f305228f85cd2491c840985263704efbde83f
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-06-05 18:53:58 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
   variable: "--font-inter",
 })
 
-import { Sidebar } from "@/components/ui/navigation/sidebar"
+import { Sidebar } from "@/components/ui/navigation/Sidebar"
 import { siteConfig } from "./siteConfig"
 
 export const metadata: Metadata = {
