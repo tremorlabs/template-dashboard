e6aff361e05b715403bfbd8faf9e8d9f4e39ad0f
severinlandolt <sev.landolt@gmail.com>
2024-06-05 19:40:52 +0200

    fix preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { siteConfig } from "./siteConfig"
 
-import { Sidebar } from "@/components/ui/navigation/Sidebar"
 import { ArrowAnimated } from "@/components/ui/icons/ArrowAnimated"
+import { Sidebar } from "@/components/ui/navigation/Sidebar"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -44,15 +44,18 @@ function Banner() {
     <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2 transition">
       <div className="flex items-center gap-x-1 rounded-full bg-gray-950 p-1 text-sm shadow-xl shadow-black/20 ring-1 ring-white/10">
         <a
-          className="flex items-center gap-x-2 rounded-l-[20px] rounded-r-md bg-gray-900 py-2 pl-4 pr-2 font-semibold text-gray-50 ring-1 ring-inset ring-white/20 hover:bg-gray-800/90 hover:text-white"
+          className="flex items-center gap-x-2 whitespace-nowrap rounded-l-[20px] rounded-r-md bg-gray-900 py-2 pl-4 pr-2 font-semibold text-gray-50 ring-1 ring-inset ring-white/20 hover:bg-gray-800/90 hover:text-white"
           href="https://github.com/tremorlabs/template-dashboard-oss"
           target="_blank"
         >
-          <RiGithubFill className="-ml-1 size-5 shrink-0" aria-hidden="true" />
+          <RiGithubFill
+            className="-ml-1 hidden size-5 shrink-0 sm:flex"
+            aria-hidden="true"
+          />
           Get OSS version
         </a>
         <a
-          className="group flex items-center gap-0.5 rounded-l-md rounded-r-[20px] bg-gradient-to-b from-white to-gray-200 px-4 py-2 font-semibold text-gray-900 ring-1 ring-inset ring-indigo-400/30 transition"
+          className="group flex items-center gap-0.5 whitespace-nowrap rounded-l-md rounded-r-[20px] bg-gradient-to-b from-white to-gray-200 px-4 py-2 font-semibold text-gray-900 ring-1 ring-inset ring-indigo-400/30 transition"
           href="https://blocks.tremor.so/templates#template-dashboard"
           target="_blank"
         >
