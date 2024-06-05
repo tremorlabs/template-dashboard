1f548f59c3a584362f2899a5565b9ddbd216f3c5
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2024-06-05 16:41:05 +0200

    styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,10 +42,10 @@ function Banner() {
   return (
     <div className="fixed bottom-10 left-1/2 z-50 -translate-x-1/2 transition hover:-translate-y-px">
       <div
-        className="text-sm rounded-full flex items-center gap-x-1 bg-gray-950 p-1 py-1 shadow-xl shadow-black/20 ring-1 ring-white/10 hover:bg-gray-900"
+        className="text-sm rounded-full flex items-center gap-x-1 bg-gray-950 p-1 py-1 shadow-xl shadow-black/20 ring-1 ring-white/10"
       >
         <a
-          className="font-medium text-white py-2 pr-2 pl-4 rounded-l-full flex items-center gap-x-2"
+          className="font-semibold text-gray-50 hover:text-white py-2 pr-2 pl-4 rounded-l-full flex items-center gap-x-2"
           href="[GITHUB_REPO_LINK]"
           target="_blank"
         >
@@ -53,7 +53,7 @@ function Banner() {
           Get OSS version
         </a>
         <a
-          className="font-medium py-2 px-4 bg-indigo-600 hover:bg-indigo-500 text-white rounded-full ring-1 ring-inset ring-indigo-400/30"
+          className="font-semibold py-2 px-4 bg-indigo-600 hover:bg-indigo-500 text-white rounded-full ring-1 ring-inset ring-indigo-400/30"
           href="https://blocks.tremor.so/templates#template-dashboard"
           target="_blank"
 
